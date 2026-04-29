c211417ea9f430b66a665bc9c10ec1f77b11268b
dargereldren <dargereldren@avackusniche.org>
2026-04-29 15:02:18 -0400

    support for the 11MB SuperFX mapper

--- core/fxemu.h
+++ core/fxemu.h
@@ -10,6 +10,9 @@
 #define FX_BREAKPOINT				(-1)
 #define FX_ERROR_ILLEGAL_ADDRESS	(-2)
 
+// [dargereldren] Where in memory does the 32KB mirroring occur
+#define FX_MEMORY_32K_MIRRORS 0xC00000
+
 // The FxInfo_s structure, the link between the FxEmulator and the Snes Emulator
 struct FxInfo_s
 {
